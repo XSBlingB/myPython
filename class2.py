e1cabfcc826643bd09bd413e1cc2cbecb6777dfb
XSBlingB <brave_shine_xs@163.com>
2019-07-26 18:05:14 +0800

    add 7.26 to repo

--- class2.py
+++ class2.py
@@ -23,3 +23,27 @@ print(dog.eat())
 cat = Cat('小猫')
 print(cat.run())
 print(cat.eat())
+
+a = list()
+print(isinstance(a,list))# 判断对象类型
+print(isinstance(dog,Animal))
+print(isinstance(dog,Dog))#dog既是Dog，也是Animal的对象
+
+def run_twice(animal):
+    animal.run()
+    animal.run()#定义一个运行两次run方法的函数
+
+print(run_twice(dog))
+print(run_twice(cat))
+
+class Tortoise(Animal):
+    def eat(self):
+        print('Tortoise is eating !')
+tortoise = Tortoise('乌龟')
+
+print(run_twice(tortoise))
+#对于静态语言（例如Java）来说，如果需要传入Animal类型，则传入的对象必须是Animal类型或者它的子类，否则，将无法调用run()方法。
+
+#对于Python这样的动态语言来说，则不一定需要传入Animal类型。我们只需要保证传入的对象有一个run()方法就可以了：
+
+
